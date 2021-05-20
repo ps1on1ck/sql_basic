863ed0d451b92702592e8838c4ac48a37d075724
Alex <psionickmobile@gmail.com>
2021-05-20 10:02:30 +0300

    added hw11

---
+++ homework_11/hw11_task1.sql
@@ -0,0 +1,39 @@
+﻿-- Практическое задание по теме “Оптимизация запросов”
+--  1. Создайте таблицу logs типа Archive. Пусть при каждом создании записи в таблицах users, 
+-- catalogs и products в таблицу logs помещается время и дата создания записи, название таблицы, идентификатор первичного ключа и содержимое поля name.
+
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+  created_at DATETIME NOT NULL COMMENT 'Время и дата создания записи',
+  table_name VARCHAR(100) COMMENT 'Название таблицы',
+  id_key INT COMMENT 'Идентификатор первичного ключа',
+  name VARCHAR(255) COMMENT 'Содержимое поля name'
+) ENGINE=Archive;
+
+
+DELIMITER //
+DROP TRIGGER IF EXISTS log_users//
+CREATE TRIGGER log_users AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, id_key, name) VALUES (NOW(), 'users', NEW.id, NEW.name);
+END//
+
+DROP TRIGGER IF EXISTS log_catalogs//
+CREATE TRIGGER log_catalogs AFTER INSERT ON catalogs
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, id_key, name) VALUES (NOW(), 'catalogs', NEW.id, NEW.name);
+END//
+
+DROP TRIGGER IF EXISTS log_products//
+CREATE TRIGGER log_products AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, id_key, name) VALUES (NOW(), 'products', NEW.id, NEW.name);
+END//
+DELIMITER ;
+
+-- 2. (по желанию) Создайте SQL-запрос, который помещает в таблицу users миллион записей.
+
---
+++ homework_11/hw11_task2.sql
@@ -0,0 +1,22 @@
+﻿-- Практическое задание по теме “NoSQL”
+--  1. В базе данных Redis подберите коллекцию для подсчета посещений с определенных IP-адресов.
+SADD ip_address '192.168.0.1' '192.168.0.2' '192.168.0.3' '127.0.0.1'
+SMEMBERS ip_address
+
+-- 2. При помощи базы данных Redis решите задачу поиска имени пользователя по электронному адресу и наоборот, поиск электронного адреса пользователя по его имени
+-- 3. Организуйте хранение категорий и товарных позиций учебной базы данных shop в СУБД MongoDB.
+use catalogs
+db.catalogs.insertMany(
+  [{"name": "Процессоры"}, {"name": "Материнские платы"}, {"name": "Видеокарты"}, {"name": "Жесткие диски"}, {"name": "Оперативная память"}]
+)
+db.catalogs.find()
+
+
+use products
+db.products.insertMany([
+  {"name": "Intel Core i3-8100", "description": "Процессор для настольных ПК Intel", "price": "7890.00", "catalog_id": "Процессоры", "created_at": new Date(), "updated_at": new Date()},
+  {"name": "Intel Core i5-7400", "description": "Процессор для настольных ПК Intel", "price": "12700.00", "catalog_id": "Процессоры", "created_at": new Date(), "updated_at": new Date()},
+  {"name": "AMD FX-8320E", "description": "Процессор для настольных ПК AMD", "price": "4780.00", "catalog_id": "Процессоры", "created_at": new Date(), "updated_at": new Date()},
+  {"name": "AMD FX-8320", "description": "Процессор для настольных ПК AMD", "price": "7120.00", "catalog_id": "Процессоры", "created_at": new Date(), "updated_at": new Date()}
+])
+db.products.find().pretty()
